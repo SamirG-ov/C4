b83d606eaf96c2f552989b4ea9622d3dd39f0767
LevFRFR <47461639+LevFRFR@users.noreply.github.com>
2019-02-28 18:22:56 -0700

    Skeleton

---
+++ Enemy.java
@@ -0,0 +1,13 @@
+public class Enemy {
+	
+	private Location location;
+	private int health = 100;
+	
+	public AttackType getMove() {
+		//Player.getlocation???
+		AttackType i = AttackType.KICK;
+		return i;
+	}
+	
+	
+}
